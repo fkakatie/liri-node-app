7ec361a10bf0bd359d04b3575c1b3f3daa075048
fkakatie <sextonskatie@gmail.com>
2019-01-09 18:02:56 -0700

    first commit

---
+++ liri.js
@@ -0,0 +1,13 @@
+// LIRI HOMEWORK
+    // COLLECT INPUT FROM USER
+    // GET INFO FROM API
+    // LOG INFO TO CONSOLE
+
+// 1. search _spotify_ for song info
+
+
+
+// 2. search _bands in town_ for concert info
+
+
+// 3. search _omdb_ for movie info
