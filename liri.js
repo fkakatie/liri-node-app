d4441f4cb9135f59fbeb164fbb5d0a71b46cff36
fkakatie <sextonskatie@gmail.com>
2019-01-15 23:24:45 -0700

    concert function working

--- liri.js
+++ liri.js
@@ -10,27 +10,129 @@ require('dotenv').config();
 
 var keys = require('./keys.js');
 
+var chalk = require('chalk');
+
 var request = require('request');
 var fs = require('fs');
 var moment = require('moment');
 
 var Spotify = require('node-spotify-api');
-var BandsInTown = require('bandsintown');
+var bandsintown = require('bandsintown')('"' + keys.bandsintown + '"');
 var Omdb = require('omdb');
 
-// moment stuff
+// chalk setup
+var invalid = chalk.red;
+var title = chalk.blue;
+var divider = chalk.gray;
+var inverse = chalk.inverse;
 
-var now = moment().format();
-
-var bandsDate = moment(now).format('MM-DD-YYYY');
-
-// 1. search _spotify_ for song info
+var divLine = '-----------------------------------';
 
 var spotify = new Spotify(keys.spotify);
 
 
-// 2. search _bands in town_ for concert info
+var userInput = process.argv[2];
+// console.log(userInput);
 
+var command = {
+    // search BANDSINTOWN for concert info
+    concert: function() {
+        
+        var artist = userInput;
+        
+        var artistSplit = userInput.split('');
+  
+        for (var i = 0; i < artistSplit.length; i++) {
+            // replacing any special characters
+            switch (artistSplit[i]) {
+            case '/':
+                artistSplit[i] = '%252F';
+                break;
+            case '?':
+                artistSplit[i] = '%253F';
+                break;
+            case '*':
+                artistSplit[i] = '%252A';
+                break;
+            case '"':
+                artistSplit[i] = '%27C';
+                break;
+            default:
+                artistSplit[i] = artistSplit[i];
+                break;
+            };
 
-// 3. search _omdb_ for movie info
+        };
 
+        artistSplit = artistSplit.join('');
+        
+        request('https://rest.bandsintown.com/artists/' + artistSplit + '/events?app_id=' + keys.bandsintown + '&date=upcoming', function (error, response, bandData) {
+            // catch errors
+            if (error || response.statusCode !== 200) {
+                return console.log(invalid(error));
+            } 
+
+            if (bandData.length > 3) {
+
+                // Print the HTML for the Google homepage.
+                var bandData = JSON.parse(bandData);
+
+                console.log(title('\nUPCOMING ' + chalk.bold(artist.toUpperCase()) + ' CONCERTS:'));
+                console.log(divider(divLine));
+
+                for (var j = 0; j < 5; j++) {
+
+                    var concert = bandData[j];
+
+                    var concertDate = moment(concert.datetime).format('MM/DD/YYYY');
+
+                    console.log(
+                        concertDate + divider(' | ') + 
+                        concert.venue.city + ', ' + concert.venue.region + ' (' +
+                        concert.venue.name + ')');
+
+                }
+
+            } else {
+
+                console.log(invalid('\nIt looks like ' + artist + ' is not on tour right now.'));
+            }
+
+        });
+
+    },
+    // search SPOTIFY for song info
+    song: function() {
+        console.log('artist(s)');
+        console.log('song name');
+        console.log('album');
+        console.log('preview');
+    },
+    // search OMDB for movie info
+    movie: function() {
+        console.log('title');
+        console.log('year');
+        console.log('country');
+        console.log('language');
+        console.log('imdb rating | rotten tomatoes rating');
+        console.log('plot');
+        console.log('cast');
+    },
+    // read RANDOM.txt
+    random: function() {
+        
+        fs.readFile("random.txt", "utf8", function(error, data) {
+
+            // catch errors
+            if (error) {
+              return console.log(error);
+            }
+          
+            console.log(data);
+          
+        });
+
+    },
+};
+
+command.concert();
